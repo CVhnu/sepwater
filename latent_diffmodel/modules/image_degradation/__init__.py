0049677964f37f4e6498a6e0af5bab06f6c8ff33
CVhnu <164332852+CVhnu@users.noreply.github.com>
2025-12-11 12:53:22 +0800

    Add files via upload

---
+++ latent_diffmodel/modules/image_degradation/__init__.py
@@ -0,0 +1,2 @@
+from latent_diffmodel.modules.image_degradation.bsrgan import degradation_bsrgan_variant as degradation_fn_bsr
+from latent_diffmodel.modules.image_degradation.bsrgan_light import degradation_bsrgan_variant as degradation_fn_bsr_light
